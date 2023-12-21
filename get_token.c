52d58f591594ddd6fabe30b0dcce7ac3ecaf0e54
Diesos <ozturk@live.Fr>
2023-12-21 18:15:21 +0100

    Implement get_token function for tokenizing input

---
+++ get_token.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * tokenize - Splits a string into tokens
+ * @input: The string to be split
+ * 
+ * Return: An array of strings representing the tokens
+ */
+char **_get_token(char *input)
+{
+    char *token;
+    char **args = NULL;
+    int argCount = 0;
+    int argsSize = 10;
+
+    args = calloc(argsSize, sizeof(char *));
+
+    if (args == NULL)
+    {
+        perror("Memory allocation failed");
+        exit(1);
+    }
+
+    token = strtok(input, " \t\n");
+
+    if (token == NULL)
+    {
+        free(args);
+        return (NULL);
+    }
+
+    while (token != NULL)
+    {
+        args[argCount++] = token;
+
+        if (argCount >= argsSize)
+        {
+            argsSize *= 2;
+            args = realloc(args, argsSize * sizeof(char *));
+            if (args == NULL)
+            {
+                perror("Memory allocation failed");
+                exit(1);
+            }
+        }
+
+        token = strtok(NULL, " \t\n");
+    }
+    args[argCount] = NULL;
+
+    return (args);
+}
