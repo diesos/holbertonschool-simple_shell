3cee083a81ac978cdca956e51f66868272c013a0
antrinarich <7489@holbertonstudents.com>
2023-12-21 09:08:55 -0800

    Add declarations and prototypes to main.H

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN__H
+#define MAIN__H
+
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern char **environ;
+
+char **_get_token(char *input);
+void free_memory(char **args, char *path);
+int exec_cmd(char *input);
+char *_get_env(char *input);
+char *purge_whiteSpace(char *input);
+
+
+#endif // MAIN__H
