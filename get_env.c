10dac269652d26903e791cb4b4bc63e3f2f6beaf
Diesos <ozturk@live.Fr>
2023-12-21 18:12:42 +0100

    Implement get_env function for retrieving environment variables

---
+++ get_env.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* getEnv - get the path of an executable
+* @input: input string
+* Return: path of the executable
+*/
+char *_get_env(char *input)
+{
+	char *pathEnv, *pathCopy, *token, *result;
+	char fullPath[1024];
+
+	pathEnv = getenv("PATH");
+
+	if  (pathEnv == NULL)
+		return (NULL);
+
+	pathCopy = strdup(pathEnv);
+	token = strtok(pathCopy, ":");
+
+	while (token)
+	{
+		snprintf(fullPath, sizeof(fullPath), "%s/%s", token, input);
+
+		if (access(fullPath, F_OK | X_OK) == 0)
+		{
+			result = strdup(fullPath);
+			free(pathCopy);
+			return (result);
+		}
+		token = strtok(NULL, ":");
+	}
+	free(pathCopy);
+
+	return (NULL);
+}
